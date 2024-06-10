dc388bcc64673c917633a106f349293b3c4d4629
Mohammed Quadir <86256383+mohdquadir45@users.noreply.github.com>
2024-06-10 11:49:47 +0530

    input field added

--- client/src/components/Join.jsx
+++ client/src/components/Join.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
 
 const Join = () => {
+
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+
   return (
-    <div>Join</div>
+      <div className='joinOuterContainer'>
+          <div className='joinInnerContainer'>
+              <h1 className='heading'>Join</h1>
+              {/* input field for entering name and room */}
+              <div>
+                  <input placeholder='' className='joinInput' type='text' onChange={(event) => setName(event.target.value) } />
+              </div>
+              <div>
+                  <input placeholder='' className='' type='text' onChange={(event) => setRoom(event.target.value)}/>
+              </div>
+              {/* sign in button */}
+              <Link>
+                  <button className='button mt-20' type='submit'>Sign In</button>
+              </Link>
+          </div>
+    </div>
   )
 }
 
